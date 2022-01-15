0900b8a3005eed7b459472c0db6a6949b9a127b3
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2022-01-15 09:44:03 +0000

    add Ex3

--- tpi2.py
+++ tpi2.py
@@ -12,7 +12,6 @@ class MySemNet(SemanticNetwork):
         pass
 
     def source_confidence(self,user):
-        # IMPLEMENT HERE
         
         correct = 0
         wrong = 0
@@ -48,8 +47,9 @@ class MySemNet(SemanticNetwork):
 
                     data_point = data_dict[entity1][name]
                     max_count = max(data_point.values())
+                    max_values = [k for k,v in data_point.items() if v == max_count]
 
-                    if entity2 in [k for k,v in data_point.items() if v == max_count]:
+                    if entity2 in max_values:
                         correct += 1
                     else:
                         wrong += 1
@@ -60,9 +60,45 @@ class MySemNet(SemanticNetwork):
         conf = (1-(0.75**correct))*(0.75**wrong)
         return conf
 
-    def query_with_confidence(self,entity,assoc):
-        # IMPLEMENT HERE
-        pass
+    def query_with_confidence(self, entity, assoc):
+
+        # n = 0
+        # T = 0
+        # print('='*10)
+        # print(f'entity {entity} | assoc {assoc}')
+
+
+        # print(self.query(entity, assoc))
+        
+        # self.query_local(e1=entity, relname=assoc)
+        # self.show_query_result()
+        
+        # print(len(self.query(entity)))
+        # print('='*10)
+        
+        
+
+        return None
+
+    
+
+    def query(self, entity, assoc=None):
+        
+        # TODO remove method
+        queries_pred_b = [self.query(d.relation.entity2, assoc) for d in self.declarations if isinstance(d.relation, (Member, Subtype)) and d.relation.entity1 == entity]
+        
+        return [d for sublist in queries_pred_b for d in sublist] + self.query_local(e1=entity, relname=assoc)
+
+    # def query_local(self,user=None,e1=None,rel=None,e2=None,rel_type=None):
+
+    #     self.query_result = \
+    #         [ d for d in self.declarations
+    #             if  (user == None or d.user==user)
+    #             and (e1 == None or d.relation.entity1 == e1)
+    #             and (rel == None or d.relation.name == rel)
+    #             and (rel_type == None or isinstance(d.relation,rel_type))]
+
+    #     return self.query_result
 
 
 
@@ -74,7 +110,29 @@ class MyBN(BayesNet):
         pass
 
     def individual_probabilities(self):
-        # IMPLEMENT HERE
-        pass
+        # SOURCE: https://github.com/Rui-FMF/IA/blob/main/TPI2/tpi2.py
+
+        res = {}
+
+        variables = [k for k in self.dependencies.keys()]
+
+        for v in variables:
+            temp_vars = [k for k in self.dependencies.keys() if k != v]
+            res[v] = sum([ self.jointProb([(v, True)] + conj) for conj in self._generate_conjunctions(temp_vars) ])
+        
+        return res
+
+    def _generate_conjunctions(self, variaveis):
+        # SOURCE: https://github.com/Rui-FMF/IA/blob/main/TPI2/tpi2.py
+
+        if len(variaveis) == 1:
+            return [ [(variaveis[0], True)] , [(variaveis[0], False)] ]
+
+        l = []
+        for c in self._generate_conjunctions(variaveis[1:]):
+            l.append([(variaveis[0], True)] + c)
+            l.append([(variaveis[0], False)] + c)
+
+        return l
 
 
